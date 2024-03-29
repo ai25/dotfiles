6b42ecc890c6118210bc6af6288db68ccf49bf37
vr10t <96934297+vr10t@users.noreply.github.com>
2024-03-29 16:22:16 +0000

    update

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,109 +1,113 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
 -- set up for web development (html, css, js, ts, react, vue, svelte, php, wordpress)
 local servers = {
-  "html",
-  "cssls",
-  "tsserver",
-  "clangd",
-  "jsonls",
-  "vimls",
-  "yamlls",
-  "intelephense",
-  "svelte",
-  "vuels",
-  "tailwindcss",
-  "graphql",
-  "dockerls",
-  "bashls",
-  "jsonls",
-  "eslint",
-  "stylelint_lsp",
-  "psalm",
-  "angularls",
-  "astro",
-  "denols",
-  "rust_analyzer",
+	"html",
+	"cssls",
+	"tsserver",
+	"clangd",
+	"vimls",
+	"yamlls",
+	"intelephense",
+	"svelte",
+	"vuels",
+	"tailwindcss",
+	"graphql",
+	"dockerls",
+	"bashls",
+	"jsonls",
+	"eslint",
+	"stylelint_lsp",
+	"psalm",
+	"angularls",
+	"astro",
+	"denols",
+	"rust_analyzer",
 }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
 --
 -- eslint, prettier, stylelint
 local eslint = {
-  lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
-  lintIgnoreExitCode = true,
-  lintStdin = true,
-  lintFormats = { "%f:%l:%c: %m" },
-  formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
-  formatStdin = true,
+	lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
+	lintIgnoreExitCode = true,
+	lintStdin = true,
+	lintFormats = { "%f:%l:%c: %m" },
+	formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+	formatStdin = true,
 }
 
 local prettier = {
-  formatCommand = "prettier --stdin-filepath ${INPUT}",
-  formatStdin = true,
+	formatCommand = "prettier --stdin-filepath ${INPUT}",
+	formatStdin = true,
 }
 
 local stylelint = {
-  lintCommand = "stylelint_lsp --stdin --stdin-filename ${INPUT}",
-  lintIgnoreExitCode = true,
-  lintStdin = true,
-  lintFormats = { "%f:%l:%c: %m" },
-  formatCommand = "stylelint_lsp --fix --stdin --stdin-filename ${INPUT}",
-  formatStdin = true,
+	lintCommand = "stylelint_lsp --stdin --stdin-filename ${INPUT}",
+	lintIgnoreExitCode = true,
+	lintStdin = true,
+	lintFormats = { "%f:%l:%c: %m" },
+	formatCommand = "stylelint_lsp --fix --stdin --stdin-filename ${INPUT}",
+	formatStdin = true,
 }
 
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  init_options = { documentFormatting = true },
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" },
-  settings = {
-    rootMarkers = { ".git/" },
-    languages = {
-      javascript = { eslint, prettier },
-      javascriptreact = { eslint, prettier },
-      typescript = { eslint, prettier },
-      typescriptreact = { eslint, prettier },
-      css = { prettier, stylelint },
-      scss = { prettier, stylelint },
-      html = { prettier },
-    },
-  },
-}
-lspconfig.stylelint_lsp.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    stylelintplus = {
-      autoFixOnFormat = true,
-    },
-  },
-}
-lspconfig.eslint.setup {
-  cmd = { "eslint_d", "--stdin", "--stdin-filename", "%filepath", "--format", "json" },
-  on_attach = on_attach,
-  capabilities = capabilities,
-  -- Add additional configurations if needed
-}
-lspconfig.cssls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    css = {
-      validate = true,
-    },
-  },
-}
+lspconfig.tsserver.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	init_options = { documentFormatting = true },
+	filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" },
+	settings = {
+		rootMarkers = { ".git/" },
+		languages = {
+			javascript = { eslint, prettier },
+			javascriptreact = { eslint, prettier },
+			typescript = { eslint, prettier },
+			typescriptreact = { eslint, prettier },
+			css = { prettier, stylelint },
+			scss = { prettier, stylelint },
+			html = { prettier },
+		},
+	},
+})
+lspconfig.stylelint_lsp.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		stylelintplus = {
+			autoFixOnFormat = true,
+		},
+	},
+})
+lspconfig.eslint.setup({
+	cmd = { "eslint_d", "--stdin", "--stdin-filename", "%filepath", "--format", "json" },
+	on_attach = on_attach,
+	capabilities = capabilities,
+	-- Add additional configurations if needed
+})
+lspconfig.cssls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		css = {
+			validate = true,
+		},
+	},
+})
+lspconfig.jsonls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "vscode-json-languageserver", "--stdio" },
+})
 
 -- lspconfig.phpactor.setup {
 --   on_attach = on_attach,
@@ -115,88 +119,88 @@ lspconfig.cssls.setup {
 --   },
 -- }
 
-lspconfig.intelephense.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    intelephense = {
-      format = {
-        enable = true,
-      },
-      stubs = {
-        "apache",
-        "bcmath",
-        "bz2",
-        "calendar",
-        "com_dotnet",
-        "Core",
-        "ctype",
-        "curl",
-        "date",
-        "dba",
-        "dom",
-        "enchant",
-        "exif",
-        "FFI",
-        "fileinfo",
-        "filter",
-        "fpm",
-        "ftp",
-        "gd",
-        "gettext",
-        "gmp",
-        "hash",
-        "iconv",
-        "imap",
-        "intl",
-        "json",
-        "ldap",
-        "libxml",
-        "mbstring",
-        "meta",
-        "mysqli",
-        "oci8",
-        "odbc",
-        "openssl",
-        "pcntl",
-        "pcre",
-        "PDO",
-        "pdo_ibm",
-        "pdo_mysql",
-        "pdo_pgsql",
-        "pdo_sqlite",
-        "pgsql",
-        "Phar",
-        "posix",
-        "pspell",
-        "readline",
-        "Reflection",
-        "session",
-        "shmop",
-        "SimpleXML",
-        "snmp",
-        "soap",
-        "sockets",
-        "sodium",
-        "SPL",
-        "sqlite3",
-        "standard",
-        "superglobals",
-        "sysvmsg",
-        "sysvsem",
-        "sysvshm",
-        "tidy",
-        "tokenizer",
-        "xml",
-        "xmlreader",
-        "xmlrpc",
-        "xmlwriter",
-        "xsl",
-        "Zend OPcache",
-        "zip",
-        "zlib",
-        "wordpress",
-      },
-    },
-  },
-}
+lspconfig.intelephense.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		intelephense = {
+			format = {
+				enable = true,
+			},
+			stubs = {
+				"apache",
+				"bcmath",
+				"bz2",
+				"calendar",
+				"com_dotnet",
+				"Core",
+				"ctype",
+				"curl",
+				"date",
+				"dba",
+				"dom",
+				"enchant",
+				"exif",
+				"FFI",
+				"fileinfo",
+				"filter",
+				"fpm",
+				"ftp",
+				"gd",
+				"gettext",
+				"gmp",
+				"hash",
+				"iconv",
+				"imap",
+				"intl",
+				"json",
+				"ldap",
+				"libxml",
+				"mbstring",
+				"meta",
+				"mysqli",
+				"oci8",
+				"odbc",
+				"openssl",
+				"pcntl",
+				"pcre",
+				"PDO",
+				"pdo_ibm",
+				"pdo_mysql",
+				"pdo_pgsql",
+				"pdo_sqlite",
+				"pgsql",
+				"Phar",
+				"posix",
+				"pspell",
+				"readline",
+				"Reflection",
+				"session",
+				"shmop",
+				"SimpleXML",
+				"snmp",
+				"soap",
+				"sockets",
+				"sodium",
+				"SPL",
+				"sqlite3",
+				"standard",
+				"superglobals",
+				"sysvmsg",
+				"sysvsem",
+				"sysvshm",
+				"tidy",
+				"tokenizer",
+				"xml",
+				"xmlreader",
+				"xmlrpc",
+				"xmlwriter",
+				"xsl",
+				"Zend OPcache",
+				"zip",
+				"zlib",
+				"wordpress",
+			},
+		},
+	},
+})
--- nvim/.config/nvim/lua/custom/configs/overrides.lua
+++ nvim/.config/nvim/lua/custom/configs/overrides.lua
@@ -1,119 +1,120 @@
 local M = {}
-local mappings = require "custom.mappings"
+local mappings = require("custom.mappings")
 
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "lua",
-    "html",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "c",
-    "markdown",
-    "markdown_inline",
-    "php",
-    "c_sharp",
-    "cpp",
-    "dart",
-    "dockerfile",
-    "jsdoc",
-    "json",
-    "kotlin",
-    "nix",
-    "prisma",
-    "python",
-    "rust",
-    "scss",
-    "svelte",
-    "vue",
-    "xml",
-    "zig",
-  },
-  indent = {
-    enable = true,
-    -- disable = {
-    --   "python"
-    -- },
-  },
+	ensure_installed = {
+		"vim",
+		"lua",
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"tsx",
+		"c",
+		"markdown",
+		"markdown_inline",
+		"php",
+		"c_sharp",
+		"cpp",
+		"dart",
+		"dockerfile",
+		"jsdoc",
+		"json",
+		"kotlin",
+		"nix",
+		"prisma",
+		"python",
+		"rust",
+		"scss",
+		"svelte",
+		"vue",
+		"xml",
+		"zig",
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		--   "python"
+		-- },
+	},
 }
 
 M.mason = {
-  ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
+	ensure_installed = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
 
-    -- web dev stuff
-    "angular-language-server",
-    "astro-language-server",
-    "css-lsp",
-    "deno",
-    "eslint_d",
-    "html-lsp",
-    "prettierd",
-    "rustywind",
-    "stylelint",
-    "typescript-language-server",
+		-- web dev stuff
+		"angular-language-server",
+		"astro-language-server",
+		"css-lsp",
+		"deno",
+		"eslint_d",
+		"html-lsp",
+		"prettierd",
+		"rustywind",
+		"stylelint",
+		"typescript-language-server",
+		"json-lsp",
 
-    "markdownlint",
-    "mdx-analyzer",
-    "write-good",
+		"markdownlint",
+		"mdx-analyzer",
+		"write-good",
 
-    "htmx-lsp",
+		"htmx-lsp",
 
-    "intelephense",
-    -- "phpactor",
-    "psalm",
-    "phpcbf",
+		"intelephense",
+		-- "phpactor",
+		"psalm",
+		"phpcbf",
 
-    -- c/cpp stuff
-    "clangd",
-    "clang-format",
+		-- c/cpp stuff
+		"clangd",
+		"clang-format",
 
-    -- misc stuff
-    "codespell",
-  },
+		-- misc stuff
+		"codespell",
+	},
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-  git = {
-    enable = true,
-  },
+	git = {
+		enable = true,
+	},
 
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
 }
 
 M.copilot = {
-  -- Possible configurable fields can be found on:
-  -- https://github.com/zbirenbaum/copilot.lua#setup-and-configuration
-  suggestion = {
-    enable = false,
-  },
-  panel = {
-    enable = false,
-  },
+	-- Possible configurable fields can be found on:
+	-- https://github.com/zbirenbaum/copilot.lua#setup-and-configuration
+	suggestion = {
+		enable = false,
+	},
+	panel = {
+		enable = false,
+	},
 }
 
 M.obsidian = {
-  workspaces = {
-    {
-      name = "personal",
-      path = "~/Shared Documents/",
-    },
-  },
-  mappings = mappings.obsidian,
+	workspaces = {
+		{
+			name = "personal",
+			path = "~/Shared Documents/",
+		},
+	},
+	mappings = mappings.obsidian,
 
-  -- see below for full list of options 👇
+	-- see below for full list of options 👇
 }
 
 return M
